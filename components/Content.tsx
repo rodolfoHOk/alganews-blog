1151eca1ddfcc8d2478c88428bbd63bbeda15f97
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-20 12:19:03 -0300

    add container in Content component

--- components/Content.tsx
+++ components/Content.tsx
@@ -8,10 +8,17 @@ interface ContentProps {
 
 export default function Content(props: ContentProps) {
   return <Wrapper>
-    {props.children}
+    <Container>
+      {props.children}
+    </Container>
   </Wrapper>
 }
 
 const Wrapper = styled.div`
   min-height: calc(100vh - ${HEADER_HEIGHT}px - ${FOOTER_HEIGHT}px);
+`;
+
+const Container = styled.div`
+  max-width: 848px;
+  margin: auto;
 `;
