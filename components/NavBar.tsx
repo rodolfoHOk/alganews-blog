e389c6ed208632c9d1868e637a9cbdbcebd88cae
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-20 12:40:09 -0300

    add NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+import Link from 'next/link';
+
+export default function NavBar() {
+  return <nav>
+    <Wrapper>
+      <li>
+        <Link href="/">
+          <a>home</a>
+        </Link>
+      </li>
+    </Wrapper>
+  </nav>
+}
+
+const Wrapper = styled.ul`
+  display: flex;
+  gap: 8px;
+  list-style: none;
+
+  a {
+    color: ${p => p.theme.pageForeground};
+    text-decoration: none;
+    text-transform: lowercase;
+  }
+`;
