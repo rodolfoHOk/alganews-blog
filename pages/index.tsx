6a662d6916dbc738db773ed5774b8bf4c3eb6884
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-25 00:39:41 -0300

    define page as an optional parameter in Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ export const getServerSideProps: GetServerSideProps<HomeProps> = async (
   { query, res }
 ) => {
   const { page: _page } = query;
-  const page = Number(_page);
+  const page = _page ? Number(_page) : 1;
   
   if (isNaN(page) || page < 1) {
     return sendToHomePage(res);
