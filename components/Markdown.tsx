39b8b546f0a2e67c165e9a2b6f79481e879ab904
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-25 01:06:47 -0300

    remove comments in Markdown component

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,4 +1,3 @@
-// import { useEffect, useState } from 'react';
 import ReactMarkdown, { Components } from 'react-markdown';
 import gfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
@@ -31,14 +30,6 @@ interface MarkdownProps {
 }
 
 export default function Markdown(props: MarkdownProps) {
-  // for testing only
-  // const [body, setBody] = useState('');
-  // useEffect(() => {
-  //   fetch('https://gist.githubusercontent.com/danielbonifacio/367b357eac56e0e6f91a689d852b8287/raw/ce3fab591901e2c9ff35d0a59d864e0179ab297e/react-moderno.md')
-  //   .then(res => res.text())
-  //   .then(text => setBody(text));
-  // },[]);
-
   return (
     <ReactMarkdown
       className={"MarkdownRenderer"}
