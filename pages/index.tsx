108f7c81a0cd0ac2d66e332cdae01b42f28611a6
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-08 23:47:59 -0300

    chore: tabulation home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import type { GetServerSideProps, NextPage } from 'next';
-import Head from 'next/head';
-import { Post, PostService } from 'rodolfohiok-sdk';
-import FeaturedPost from '../components/FeaturedPost';
-import { ServerResponse } from 'http';
-import PostCard from '../components/PostCard';
-import PostsGrid from '../components/PostsGrid';
-import PageGrid from '../components/PageGrid';
-import ReactPaginate from 'react-paginate';
-import Router from 'next/router';
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import { Post, PostService } from "rodolfohiok-sdk";
+import FeaturedPost from "../components/FeaturedPost";
+import { ServerResponse } from "http";
+import PostCard from "../components/PostCard";
+import PostsGrid from "../components/PostsGrid";
+import PageGrid from "../components/PageGrid";
+import ReactPaginate from "react-paginate";
+import Router from "next/router";
 
 interface HomeProps {
   posts?: Post.Paginated;
@@ -23,51 +23,49 @@ const Home: NextPage<HomeProps> = (props: HomeProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      { 
-        posts?.content?.length &&
+      {posts?.content?.length && (
         <FeaturedPost postSummary={posts.content[0]} />
-      }
-      
+      )}
+
       <PostsGrid>
-        {
-          posts?.content?.slice(1).map(post => (
-            <PostCard key={post.id} post={post}/>
-          ))
-        }
+        {posts?.content?.slice(1).map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
       </PostsGrid>
 
-      <ReactPaginate 
+      <ReactPaginate
         containerClassName="Pagination"
         pageCount={posts?.totalPages || 0}
         marginPagesDisplayed={0}
         pageRangeDisplayed={3}
-        previousLabel={'<'}
-        nextLabel={'>'}
-        hrefBuilder={page => `/?page=${page}`}
-        onPageChange={page => {
+        previousLabel={"<"}
+        nextLabel={">"}
+        hrefBuilder={(page) => `/?page=${page}`}
+        onPageChange={(page) => {
           Router.push(`/?page=${page.selected + 1}`);
         }}
       />
     </PageGrid>
-  )
-}
+  );
+};
 
 export default Home;
 
 function sendToHomePage(res: ServerResponse) {
   res.statusCode = 302;
-  res.setHeader('Location', '/?page=1');
+  res.setHeader("Location", "/?page=1");
   return {
-    props:{}
+    props: {},
   };
 }
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async (
-  { query, res }
-) => {
+export const getServerSideProps: GetServerSideProps<HomeProps> = async ({
+  query,
+  res,
+}) => {
   const { page: _page } = query;
   const page = _page ? Number(_page) : 1;
-  
+
   if (isNaN(page) || page < 1) {
     return sendToHomePage(res);
   }
@@ -81,6 +79,6 @@ export const getServerSideProps: GetServerSideProps<HomeProps> = async (
   return {
     props: {
       posts,
-    }
-  }
-}
+    },
+  };
+};
